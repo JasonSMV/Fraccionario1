b2f2dcbccbc63c9de15173fe07b527e106c57c1f
jasonsmv <jeisonsmv@gmail.com>
2021-08-08 20:05:15 -0500

    Fecha y Proyectil

---
+++ src/com/company/Fecha.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Fecha {
+
+    private String año;
+    private String mes;
+    private String dia;
+
+    public String getAño() {
+        return año;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setAño(String año) {
+        this.año = año;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+}
---
+++ src/com/company/Proyectil.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Proyectil {
+
+    private double velocidad;
+    private double angulo;
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public double getAngulo() {
+        return angulo;
+    }
+
+    public void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public void setAngulo(double angulo) {
+        this.angulo = angulo;
+    }
+}
