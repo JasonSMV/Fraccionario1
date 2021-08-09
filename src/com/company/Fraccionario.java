35bbb49c38f923eaaca0cea50f1ae4452fef4a74
jasonsmv <jeisonsmv@gmail.com>
2021-08-08 19:52:55 -0500

    Se agrega el proyecto

---
+++ src/com/company/Fraccionario.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Fraccionario {
+
+    private int numerador;
+    private int denominador;
+
+    public void setNumerador(int numerador){
+        this.numerador = numerador;
+    }
+
+    public void setDenominador(int denominador){
+        this.denominador = denominador;
+    }
+
+    public int getNumerador(){
+        return this.numerador;
+    }
+
+    public int getDenominador(){
+        return this.denominador;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Fraccionario unMedio = new Fraccionario();
+        unMedio.setNumerador(1);
+        unMedio.setDenominador(2);
+
+    }
+}
